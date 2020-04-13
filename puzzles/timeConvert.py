170998a5366e70d3b3ad4d5d8c683bd038f9b25c
henri-priest <abc@example.com>
2020-04-13 11:39:34 -0500

    Move to function

--- puzzles/timeConvert.py
+++ puzzles/timeConvert.py
@@ -1,13 +1,19 @@
 import sys
 
-n = len(sys.argv)
+def timeCovert(time, offset):
+    print("Arg 1:" + arg1 + " Type: " + str(type(arg1)))
+    print("Arg 2:" + arg2 + " Type: " + str(type(arg2)))
 
-if n != 3:
-    print("Requires 2 arguments")
-    exit(1)
 
-arg1 = sys.argv[1]
-arg2 = sys.argv[2]
+if __name__ == "__main__":
 
-print("Arg 1:" + arg1 + " Type: " + str(type(arg1)))
-print("Arg 2:" + arg2 + " Type: " + str(type(arg2)))
+    n = len(sys.argv)
+
+    if n != 3:
+        print("Requires 2 arguments")
+        exit(1)
+
+    arg1 = sys.argv[1]
+    arg2 = sys.argv[2]
+
+    timeCovert(arg1, arg2)
