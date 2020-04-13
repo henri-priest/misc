cc19d991955499f08b766016bd694802466ba8ed
henri-priest <abc@example.com>
2020-04-13 13:51:27 -0500

    Basic conversion works

--- puzzles/addMinutes.py
+++ puzzles/addMinutes.py
@@ -3,6 +3,9 @@ import re
 
 def addMinutes(time, offset):
 
+    # 720 minutes per 12 hours
+    min_per_12hrs = 720
+
     # Enforce the time stamp pattern on the time argument
     if not re.match(r"^([1-9]|1[0,1,2]):([0-5][0-9]) [A,P]M$", time):
         print("Enter time in format [H]H:MM {AM|PM}")
@@ -11,8 +14,27 @@ def addMinutes(time, offset):
     # Enforce that offset is an integer
     offset = int(offset)
 
-    print("Arg 1:" + time + " Type: " + str(type(time)))
-    print("Arg 2:" + str(offset) + " Type: " + str(type(offset)))
+    print("Time: " + time + " , Type: " + str(type(time)))
+    print("Offset: " + str(offset) + " , Type: " + str(type(offset)))
+
+    hours = time.split(' ')[0].split(":")[0]
+    minutes = time.split(' ')[0].split(":")[1]
+    total_min = int(hours) * 60 + int(minutes)
+    new_min = total_min + offset
+
+    ampm = time.split(" ")[1]
+    am_pm_flips = int(total_min / min_per_12hrs)
+
+    new_time = new_min % min_per_12hrs
+    new_hour = int( new_time / 60)
+    new_min = new_time % 60
+
+    print("New hour: " + str(new_hour))
+    print("New minute: " + str(new_min))
+    print("AM/PM flips: " + str(am_pm_flips))
+
+    final_time = str(new_hour) + ":" + str(new_min).zfill(2)
+    print("New time: " + final_time)
 
 if __name__ == "__main__":
 
