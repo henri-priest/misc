1b2ee997d1e3d81585ea7ba50f025720c41b2db5
henri-priest <abc@example.com>
2020-04-13 12:03:40 -0500

    Finalize regex

--- puzzles/timeConvert.py
+++ puzzles/timeConvert.py
@@ -1,9 +1,15 @@
 import sys
 import re
 
-def timeCovert(time, offset):
-    print("Arg 1:" + arg1 + " Type: " + str(type(arg1)))
-    print("Arg 2:" + arg2 + " Type: " + str(type(arg2)))
+def addMinutes(time, offset):
+
+    if re.match(r"^[0-9]{1,2}:[0-9]{2} [A,P]M$", time):
+        print("Matches regex")
+    else:
+        print("Does not match regex")
+
+    print("Arg 1:" + time + " Type: " + str(type(time)))
+    print("Arg 2:" + offset + " Type: " + str(type(offset)))
 
 if __name__ == "__main__":
 
@@ -13,12 +19,7 @@ if __name__ == "__main__":
         print("Requires 2 arguments")
         exit(1)
 
-    arg1 = sys.argv[1]
-    arg2 = sys.argv[2]
+    time = sys.argv[1]
+    offset = sys.argv[2]
 
-    if re.match(r"[0-9]{1,2}:[0-9]{2} +", arg1):
-        print("Matches regex")
-    else:
-        print("Does not match regex")
-
-    timeCovert(arg1, arg2)
+    addMinutes(time, offset)
