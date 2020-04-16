124a970311fe1208f29fbdf9af0a4097ca8d9554
henri-priest <abc@example.com>
2020-04-16 12:31:47 -0500

    more tests

--- puzzles/addMinutes.py
+++ puzzles/addMinutes.py
@@ -69,6 +69,9 @@ def runTests():
 
     assert addMinutes("11:59 AM", 1) == "12:00 PM"
     assert addMinutes("11:59 AM", 2) == "12:01 PM"
+    assert addMinutes("12:00 AM", 0) == "12:00 AM"
+    assert addMinutes("12:00 AM", -1) == "11:59 PM"
+    assert addMinutes("11:59 AM", 2) == "12:01 PM"
     assert addMinutes("12:00 AM", -720) == "12:00 PM"
     assert addMinutes("12:00 AM", 720) == "12:00 PM"
     assert addMinutes("12:00 AM", 1439) == "11:59 PM"
