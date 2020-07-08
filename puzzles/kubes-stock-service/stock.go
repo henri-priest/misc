170d3e2a9c6eb3a787937e8dfe89ac5de2d27005
unknown <henri.priest@gmail.com>
2020-07-07 21:45:52 -0500

    test go wit env vars

--- puzzles/kubes-stock-service/stock.go
+++ puzzles/kubes-stock-service/stock.go
@@ -1,7 +1,16 @@
 package main
 
-import "fmt"
+import (
+    "fmt"
+    "os"
+    //"strings"
+    //"net/url"
+)
 
 func main() {
-    fmt.Println("hello world")
+    fmt.Println("Querying Stock API...")
+    fmt.Println("Stock:", os.Getenv("SYMBOL"))
+    fmt.Println("Days:", os.Getenv("NDAYS"))
+    //s := "postgres://user:pass@host.com:5432/path?k=v#f"
+    //res, err := url.Parse(s)
 }
