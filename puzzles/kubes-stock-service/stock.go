0ba86fd8f13264eb837b981e3ccd37a72ec9d041
unknown <henri.priest@gmail.com>
2020-07-07 23:20:08 -0500

    API call works in go

--- puzzles/kubes-stock-service/stock.go
+++ puzzles/kubes-stock-service/stock.go
@@ -4,6 +4,7 @@ import (
     "io/ioutil"
     "fmt"
     "os"
+    "log"
     "net/http"
 )
 
@@ -11,16 +12,22 @@ func main() {
     fmt.Println("Querying Stock API...")
     var symbol  string = os.Getenv("SYMBOL")
     //var days string = os.Getenv("NDAYS")
+
     site := fmt.Sprintf("https://www.alphavantage.co/query?apikey=1123&function=TIME_SERIES_DAILY_ADJUSTED&symbol=%s", symbol)
     fmt.Println(site)
-    res, err := http.Get(site)
 
+    res, err := http.Get(site)
     if err != nil {
-        panic(err)
+        log.Fatal(err)
+    }
+    defer res.Body.Close()
+
+    content, err := ioutil.ReadAll(res.Body)
+    if err != nil {
+        log.Fatal(err)
     }
 
-    defer res.Body.Close()
-    body, err := ioutil.ReadAll(res.Body)
-    fmt.Println(body)
+    responseString := string(content)
+    fmt.Println(responseString)
 
 }
