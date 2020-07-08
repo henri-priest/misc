ee3a52ebbe4b754f1640f61d7872c3b916f26f6c
unknown <henri.priest@gmail.com>
2020-07-07 22:25:46 -0500

    go file sorta works

--- puzzles/kubes-stock-service/stock.go
+++ puzzles/kubes-stock-service/stock.go
@@ -1,17 +1,26 @@
 package main
 
 import (
+    //"io/ioutil"
     "fmt"
     "os"
-    //"strings"
-    //"net/url"
+    "net/http"
 )
 
 func main() {
     fmt.Println("Querying Stock API...")
     var symbol  string = os.Getenv("SYMBOL")
     //var days string = os.Getenv("NDAYS")
-    url := fmt.Sprintf("https://www.alphavantage.co/query?apikey=1123function=TIME_SERIES_DAILY_ADJUSTED&symbol=%s", symbol)
-    fmt.Println(url)
-    //res, err := url.Parse(s)
+    site := fmt.Sprintf("https://www.alphavantage.co/query?apikey=1123function=TIME_SERIES_DAILY_ADJUSTED&symbol=%s", symbol)
+    fmt.Println(site)
+    res, err := http.Get(site)
+
+    if err != nil {
+        panic(err)
+    }
+
+    defer res.Body.Close()
+    //body, err := ioutil.ReadAll(res.Body)
+    fmt.Println(res.Body)
+
 }
